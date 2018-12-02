6fd33e2124c1b470fb085500fa0cb186ec725f43
boosh <boosh@sugarkube.io>
2018-12-02 16:51:57 +0000

    - Add some demo site data for the two wordpress sites

---
+++ demo-data/wordpress/site1/data.sql
@@ -0,0 +1,2 @@
+update wp_options set option_value="Site 1" where option_name ="blogname";
+update wp_options set option_value="Created with Sugarkube" WHERE option_name="blogdescription";
---
+++ demo-data/wordpress/site2/data.sql
@@ -0,0 +1,2 @@
+update wp_options set option_value="Site 2" where option_name ="blogname";
+update wp_options set option_value="Another site created with Sugarkube" WHERE option_name="blogdescription";
